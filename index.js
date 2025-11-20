7b46114693531e6f2eedea64f704afe63d710688
Rodrigo Emanuel Gomez Muñoz <emanuelmunoz695@gmail.com>
2025-11-19 23:24:28 -0300

    Se corrige la  variable de sesión

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     cookie: {
-        secure: true,
+        secure: false,
         httpOnly: true,
         sameSite: 'lax',
         maxAge: 1000 * 60 * 30
